924f2609b43b51c979543d3905f6b4899760b7b8
LuciLua <luci.lua81@gmail.com>
2021-09-23 20:39:17 -0300

    :cactus: profileMenuOpenClose colorHeaderFooter

--- js/app.js
+++ js/app.js
@@ -1,24 +1,4 @@
-const axios = require('axios')
-const baseURL = 'https://www.googleapis.com/youtube/v3/search'
-
-function searchByKeyword() {
-    var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-    for(var i in results.items) {
-      var item = results.items[i];
-      Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-    }
-}
-
-axios.get(baseURL).then((res) => {
-    console.log(res.data.channelId === 'UCIbJuoAAdTP9rClO7mK-aVg')
-})
-
-
-
-
-// var axios = require('axios');
-// const base_url = 'http://'
-
-// axios.get(base_url).then((res) => {
-// console.log(res.data)
-// })
+const profileOpen = () => {
+    var tab = document.querySelector('.closed')
+    tab.classList.toggle('open')
+} 
