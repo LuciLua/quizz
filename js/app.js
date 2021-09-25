a7c8d65c3f14f191c084402f92fe77a87797ff5f
LuciLua <luci.lua81@gmail.com>
2021-09-25 00:23:30 -0300

    :cactus: active res add className

--- js/app.js
+++ js/app.js
@@ -1,4 +1,31 @@
-const profileOpen = () => {
+function profileOpen() {
     var tab = document.querySelector('.closed')
     tab.classList.toggle('open')
 } 
+
+function active() {
+    // for(let i = 0; i < resposta.length; i++){
+    //     resposta[i].classList.add('active')
+    //     console.log('ola', resposta[i], i)
+console.log('ola')
+    //}
+}
+
+
+// respostas.addEventListener('click', active())
+
+// window.addEventListener('keydown', event => 
+// {
+//     if(event.key === '1'){
+//         var respostas = document.querySelectorAll(".resposta")
+//         respostas.forEach(e => e.classList.add('active'))
+//     }
+// })
+
+
+function active(e){
+   e.classList.add('active')
+}
+
+var respostas = document.querySelectorAll(".resposta")
+respostas.forEach(e => e.addEventListener('click', () => active(e)))
